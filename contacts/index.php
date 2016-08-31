9dc6ab53f6f36bbde41ce21eb363740d93be7d1d
Roman Kulichkov <roman@kulichkov.pro>
2016-08-31 13:19:19 +0700

    +раздел контакты

---
+++ contacts/index.php
@@ -0,0 +1,10 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Контакты");
+?><p>ООО «Сибирь-комплект»<br>
+<span style="background-color: initial;">630124, г. Новосибирск,&nbsp;ул. Куприна, д. 8/1, офис 3</span><br><span style="background-color: initial;">Тел./факс: (383) 203-88-88 (многоканальный)</span></p>
+<div class="contacts-map">
+  <a href="http://maps.2gis.ru/#/?history=project/novosibirsk/center/82.990400060875,55.044043081427/zoom/16/state/widget/id/141265769355871/firms/141265769355871" id="firmsonmap_biglink">Перейти к большой карте</a> <script charset="utf-8" type="text/javascript" src="http://firmsonmap.api.2gis.ru/js/DGWidgetLoader.js"></script><script charset="utf-8" type="text/javascript">new DGWidgetLoader({"width":"100%","height":"427","wid":"582b9b646a2231a19f69c7f87c35d156","pos":{"lon":"82.990400060875","lat":"55.044043081427","zoom":"16"},"opt":{"ref":"hidden","card":["name","contacts","schedule"],"city":"novosibirsk"},"org":[{"id":"141265769355871"}]});</script><br><br>
+</div><br>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
